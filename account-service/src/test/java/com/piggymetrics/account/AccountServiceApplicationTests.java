e6dd0f29441d3c7da50aad86fef479baa3459abb
dqminh2810 <dqminh2810@gmail.com>
2023-10-11 14:41:36 +0200

    update build and deploy config

--- account-service/src/test/java/com/piggymetrics/account/AccountServiceApplicationTests.java
+++ account-service/src/test/java/com/piggymetrics/account/AccountServiceApplicationTests.java
@@ -2,12 +2,12 @@ package com.piggymetrics.account;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = AccountServiceApplication.class)
+@ActiveProfiles(profiles = { "test" })
 class AccountServiceApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
